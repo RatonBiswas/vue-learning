dd1622d07d7943e4a4e996a19cdc90c79f8ce42b
Raton Biswas <raton35-2241@diu.edu.bd>
2023-07-23 14:14:53 +0600

    adding some core functionality

--- 1.vue-tuto/monster-slayer/app.js
+++ 1.vue-tuto/monster-slayer/app.js
@@ -13,9 +13,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
@@ -43,6 +49,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startNewGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -69,6 +81,9 @@ const app = Vue.createApp({
       }
       this.attackPlayer();
       // this.healMonster();
+    },
+    surrender() {
+      this.winner = "monster";
     },
     // healMonster() {
     //   const healValue = getRandomValue(8, 20);

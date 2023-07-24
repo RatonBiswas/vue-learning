c62dd253ba6139c7ebcd0532ffd94b391ed037c5
Raton Biswas <raton35-2241@diu.edu.bd>
2023-07-24 10:54:53 +0600

    Create contact list using props,binding.

---
+++ 1.vue-tuto/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,61 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavourite ? 'Favourite' : '' }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <button @click="toggleFavourites">Favourite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone : </strong>{{ phoneNumber }}</li>
+      <li><strong>Email : </strong>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  //   props: ['name', 'phoneNumber', 'emailAddress'],
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    // isFavourite: {
+    //   type: String,
+    //   required: true,
+    //   default: '0',
+    //   validate: function (value) {
+    //     return value === '1' || value === '0'
+    //   }
+    // }
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavourite: this.isFavourite
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    },
+    toggleFavourites() {
+      this.friendIsFavourite = !this.friendIsFavourite
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>

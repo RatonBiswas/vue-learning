96c4e7bce35a84bf6cdef456945a0d6e9bc92d97
Raton Biswas <raton35-2241@diu.edu.bd>
2023-07-29 18:43:20 +0600

    initial setup

---
+++ 1.vue-tuto/coach/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <h2>Hi Raton!</h2>
+  </div>
+</template>
+  
+<script>
+</script>
+  
+  <style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>

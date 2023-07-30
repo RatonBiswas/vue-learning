8905b01a44791fccc4acd59f4a1bb582333cbe0e
Raton Biswas <raton35-2241@diu.edu.bd>
2023-07-30 18:12:40 +0600

    Registering as a Coach The Form

--- 1.vue-tuto/coach/src/pages/coaches/CoachRegistration.vue
+++ 1.vue-tuto/coach/src/pages/coaches/CoachRegistration.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
-        register
-    </div>
+  <section>
+    <base-card>
+      <h2>Register as a coach now</h2>
+      <CoachForm />
+    </base-card>
+  </section>
 </template>
 
 <script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
 export default {
-   
+  components: {
+    CoachForm
+  }
 }
 </script>
 
 <style scoped>
-
 </style>

aaa3dd7e918ea71e64382a60eab761ef613de560
Raton Biswas <raton35-2241@diu.edu.bd>
2023-07-30 11:26:42 +0600

    Adding BaseCmps Base Card, Base Button, Base Badge

--- 1.vue-tuto/coach/src/pages/coaches/CoachDetails.vue
+++ 1.vue-tuto/coach/src/pages/coaches/CoachDetails.vue
@@ -1,17 +1,35 @@
 <template>
-    <div>
-        details for coach
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fulName }}</h2>
+        <h3>${{ rate }} hour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach Out Now!</h2>
+          <base-button link :to="contachLink">Contact</base-button>
+        </header>
         <router-view></router-view>
-        <router-link to="/coaches/ci/contact">Contact</router-link>
-    </div>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge v-for="area in areas" :key="area" :type="area" :title="area">
+          <p>{{ description }}</p>
+        </base-badge>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-   
+    props:['id']
 }
 </script>
 
 <style scoped>
-
 </style>
--- 1.vue-tuto/coach/src/pages/coaches/CoachesList.vue
+++ 1.vue-tuto/coach/src/pages/coaches/CoachesList.vue
@@ -2,22 +2,24 @@
   <div>
     <section>filter</section>
     <section>
+      <base-card>
       <div class="controls">
-        <button>Refresh</button>
-        <router-link to="/register">Register As Coach</router-link>
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register As Coach</base-button>
       </div>
       <ul v-if="hasCoaches">
         <CoachesItem
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :first-name="coach.firstName"
-          :last-name="coach.lastName"
-          :rate="coach.hourlyRate"
-          :areas="coach.areas"
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
         />
       </ul>
       <h3 v-else>No coaches found</h3>
+    </base-card>
     </section>
   </div>
 </template>

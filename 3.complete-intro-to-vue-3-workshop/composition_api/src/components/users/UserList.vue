a31b9b22e183e3ccd30edd9df9815833a8d0540d
Raton Biswas <raton35-2241@diu.edu.bd>
2023-08-07 00:27:27 +0600

    Migrating a Big Component

--- 3.complete-intro-to-vue-3-workshop/composition_api/src/components/users/UserList.vue
+++ 3.complete-intro-to-vue-3-workshop/composition_api/src/components/users/UserList.vue
@@ -20,66 +20,121 @@
   </base-container>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch } from 'vue'
 import UserItem from './UserItem.vue'
 
-export default {
-  components: {
-    UserItem
-  },
-  props: ['users'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-      sorting: null
-    }
-  },
-  computed: {
-    availableUsers() {
-      let users = []
-      if (this.activeSearchTerm) {
-        users = this.users.filter((usr) => usr.fullName.includes(this.activeSearchTerm))
-      } else if (this.users) {
-        users = this.users
-      }
-      return users
-    },
-    displayedUsers() {
-      if (!this.sorting) {
-        return this.availableUsers
-      }
-      return this.availableUsers.slice().sort((u1, u2) => {
-        if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
-          return 1
-        } else if (this.sorting === 'asc') {
-          return -1
-        } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
-          return -1
-        } else {
-          return 1
-        }
-      })
-    }
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val
-    },
-    sort(mode) {
-      this.sorting = mode
-    }
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val
-        }
-      }, 300)
-    }
+const props = defineProps(['users'])
+const emit = defineEmits(['list-projects'])
+
+const enteredSearchTerm = ref('')
+const activeSearchTerm = ref('')
+const availableUsers = computed(() => {
+  let newUsers = []
+  if (activeSearchTerm.value) {
+    newUsers = props.users.filter((usr) => usr.fullName.includes(activeSearchTerm.value))
+  } else if (props.users) {
+    newUsers = props.users
   }
+  return newUsers
+})
+
+const sorting = ref('null')
+const displayedUsers = computed(() => {
+  if (!sorting.value) {
+    return availableUsers.value
+  }
+  return availableUsers.value.slice().sort((u1, u2) => {
+    if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+      return 1
+    } else if (sorting.value === 'asc') {
+      return -1
+    } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+      return -1
+    } else {
+      return 1
+    }
+  })
+})
+
+watch(enteredSearchTerm, (newValue) => {
+  setTimeout(() => {
+    if (newValue === enteredSearchTerm.value) {
+      activeSearchTerm.value = val
+    }
+  }, 300)
+})
+
+const updateSearch = (val) => {
+  enteredSearchTerm.value = val
 }
+
+const sort = (mode) => {
+  sorting.value = mode
+}
+</script>
+
+
+<script>
+// import UserItem from './UserItem.vue'
+
+// export default {
+// components: {
+//   UserItem
+// },
+// props: ['users'],
+// data() {
+//   return {
+//     enteredSearchTerm: '',
+//     activeSearchTerm: '',
+//     sorting: null
+//   }
+// },
+// computed: {
+// availableUsers() {
+//   let users = []
+//   if (this.activeSearchTerm) {
+//     users = this.users.filter((usr) => usr.fullName.includes(this.activeSearchTerm))
+//   } else if (this.users) {
+//     users = this.users
+//   }
+//   return users
+// },
+// displayedUsers() {
+//   if (!this.sorting) {
+//     return this.availableUsers
+//   }
+//   return this.availableUsers.slice().sort((u1, u2) => {
+//     if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
+//       return 1
+//     } else if (this.sorting === 'asc') {
+//       return -1
+//     } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
+//       return -1
+//     } else {
+//       return 1
+//     }
+//   })
+// }
+// },
+// methods: {
+//   updateSearch(val) {
+//     this.enteredSearchTerm = val
+//   },
+//   sort(mode) {
+//     this.sorting = mode
+//   }
+// },
+//   watch: {
+//     enteredSearchTerm(val) {
+//       setTimeout(() => {
+//         if (val === this.enteredSearchTerm) {
+//           this.activeSearchTerm = val
+//         }
+//       }, 300)
+//     }
+//   }
+// }
 </script>
 
 <style scoped>
